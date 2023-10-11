44fb901500340280f08a1b2d61c7cd68c80f8762
David Ouellette <david.ouellette12@protonmail.com>
2023-10-11 12:15:54 -0400

    feat: routing for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'country-detail', component: CountryDetailComponent}
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: 'country-detail', component: CountryDetailComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
